85b2f9d0809285f37ff72931658940d4f5adc0e1
spmfd <110492974+spmfd@users.noreply.github.com>
2022-12-21 15:28:05 -0800

    Worked on Usercontroller and User Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,30 @@ const { User } = require('../models')
 
 module.exports = {
     // Get all users
-
+    getUsers(req, res) {
+        User.find()
+        .select("-__v")
+        .then((userdata) => res.json(userdata))
+        .catch((err) => res.status(500).json(err))
+    },
     // Get a single user by its ID and populated thought and friend data
-
+    getUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .populate([
+            { path: "thoughts", select: "-__v" },
+            { path: "friends", select: "-__v" },
+          ])
+          .select("-__v")
+          .then((user) => {
+          if (!user) {
+            res.status(404).json({ message: "User not found"})
+            return;
+          }
+          res.json(user)
+        }) .catch((err) => {
+            res.status(404).json(err)
+        })
+    },
     // Post a new User
     createUser(req, res) {
         User.create(req.body).then(newUser => res.json(newUser)).catch( err => {
@@ -27,7 +48,13 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     // Delete a User by their ID
-    
+    deleteUser (req, res) {
+        User.findOneAndDelete(
+            { _id: req.params.userId } 
+        )
+        .then((userData) => res.json(userData))
+        .catch((err) => res.status(400).json(err))
+    },
     // Add a new friend
     addFriend(req, res) {
         User.findOneAndUpdate(
@@ -40,7 +67,16 @@ module.exports = {
                 : res/json(updatedUser)
             )
             .catch((err) => res.status(500).json(err));
-    }
+    },
 
     // Delete a friend from user list
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }  
+        )
+        .then((res) => res.json(res))
+        .catch((err) => res.status(500).json(err));
+    }
 };
